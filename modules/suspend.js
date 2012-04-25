bebc15ef3796b5e4c0036383d8cd5f13e2c2164a
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2012-04-26 02:37:04 +0900

    Define default preferences

--- modules/suspend.js
+++ modules/suspend.js
@@ -1,3 +1,38 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Suspend Tab.
+ *
+ * The Initial Developer of the Original Code is SHIMODA Hiroshi.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):: SHIMODA Hiroshi <piro.outsider.reflex@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 load('lib/WindowManager');
 
 var timer = require('lib/jstimer');
@@ -17,10 +52,12 @@ var internalSS = (function() {;
 	return new ns.SessionStoreService();
 })();
 
-exports = {
+var SuspendTabConst = require('const');
+
+var exports = {
 	isSuspended : function(aTab)
 	{
-		return SS.getTabValue(aTab, 'suspendtab-state');
+		return SS.getTabValue(aTab, SuspendTabConst.STATE);
 	},
 
 	suspend : function(aTab)
@@ -36,7 +73,7 @@ exports = {
 			storate : state.storage || null,
 			index	 : state.index
 		};
-		SS.setTabValue(aTab, 'suspendtab-state', JSON.stringify(partialState));
+		SS.setTabValue(aTab, SuspendTabConst.STATE, JSON.stringify(partialState));
 
 		var browser = aTab.linkedBrowser;
 		browser.addEventListener('load', function() {
@@ -47,7 +84,7 @@ exports = {
 			}, 0);
 			if (SHistory.count > 0) SHistory.PurgeHistory(SHistory.count);
 		}, true);
-		browser.loadURI('about:blank?unloaded');
+		browser.loadURI('about:blank');
 		var SHistory = browser.sessionHistory;
 	},
 
@@ -65,10 +102,10 @@ exports = {
 	{
 		if (!this.isSuspended(aTab)) return;
 
-		var state = SS.getTabValue(aTab, 'suspendtab-state');
+		var state = SS.getTabValue(aTab, SuspendTabConst.STATE);
 		if (!state) return;
 		state = JSON.parse(state);
-		SS.setTabValue(aTab, 'suspendtab-state', '');
+		SS.setTabValue(aTab, SuspendTabConst.STATE, '');
 
 		var browser = aTab.linkedBrowser;
 		var SHistory = browser.sessionHistory
@@ -85,8 +122,8 @@ exports = {
 		});
 
 		if (internalSS._deserializeSessionStorage &&
-				state.storage &&
-				browser.docShell instanceof Ci.nsIDocShell)
+			state.storage &&
+			browser.docShell instanceof Ci.nsIDocShell)
 			internalSS._deserializeSessionStorage(state.storage, browser.docShell);
 
 		var index = (state.index || state.entries.length) - 1;
