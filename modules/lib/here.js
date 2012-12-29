b289f5462732e7820b37e8e49b53ed2972e4fab7
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2012-12-29 10:16:36 +0900

    Update library

--- modules/lib/here.js
+++ modules/lib/here.js
@@ -1,7 +1,7 @@
 /**
  * @fileOverview Here-document module for restartless addons
  * @author       YUKI "Piro" Hiroshi
- * @version      1
+ * @version      2
  * @description  Inspired from https://github.com/cho45/node-here.js
  *
  * @license
@@ -12,12 +12,49 @@
 
 const EXPORTED_SYMBOLS = ['here'];
 
+var cache = {};
+
 function here() {
 	var caller = Components.stack.caller;
 	var filename = caller.filename.split(' -> ').slice(-1)[0];
+	var line = caller.lineNumber-1;
+	var key = filename + ':' + line;
+	if (key in cache) return cache[key];
+
 	var source = read(filename);
-	var part = source.split(/\r?\n/).slice(caller.lineNumber).join('\n');
+	var part = source.split(/\r?\n/).slice(line).join('\n');
 	part = part.replace(/.*\bhere\([^\/]*\/\*/, '');
 	part = part.split('*/')[0];
+	cache[key] = part;
 	return part;
+}
+
+function shutdown() {
+	cache = undefiend;
+}
+
+if (typeof read == 'undefined') {
+	var Cc = Components.classes;
+	var Ci = Components.interfaces;
+	var IOService = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
+
+	read = function read(aURI) {
+		var uri = IOService.newURI(aURI, null, null);
+		var channel = IOService.newChannelFromURI(uri);
+		var stream = channel.open();
+
+		var fileContents = null;
+		try {
+			var scriptableStream = Cc['@mozilla.org/scriptableinputstream;1']
+					.createInstance(Ci.nsIScriptableInputStream);
+			scriptableStream.init(stream);
+			fileContents = scriptableStream.read(scriptableStream.available());
+			scriptableStream.close();
+		}
+		finally {
+			stream.close();
+		}
+
+		return fileContents;
+	};
 }
