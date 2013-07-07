aaf50cab709117f32896d2851a2b165a220ead95
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2013-07-07 22:16:20 +0900

    Add YosukeM to the list of contributors

--- modules/suspendtab.js
+++ modules/suspendtab.js
@@ -18,6 +18,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):: YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
+ *                  YosukeM (Yosuke Morimoto) https://github.com/YosukeM
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
