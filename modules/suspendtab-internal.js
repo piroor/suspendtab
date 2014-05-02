ff68be06e9c2e151fb40d1e623c05ea439d08889
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2014-05-02 18:38:08 +0900

    Works on Firefox 29 and Nightly 32.0a1.
     * Split implementation for 24ESR and others.
     * Generally, works only on Firefox 29 and later.

---
+++ modules/suspendtab-internal.js
@@ -0,0 +1,341 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var EXPORTED_SYMBOLS = ['SuspendTabInternal'];
+
+load('lib/prefs');
+load('lib/here');
+var timer = require('lib/jstimer');
+
+var SS = Cc['@mozilla.org/browser/sessionstore;1']
+			.getService(Ci.nsISessionStore);
+
+var { SessionStoreInternal } = Cu.import('resource:///modules/sessionstore/SessionStore.jsm', {});
+var { TAB_STATE_NEEDS_RESTORE } = Cu.import('resource:///modules/sessionstore/SessionStore.jsm', {});
+//var { TabRestoreStates } = Cu.import('resource:///modules/sessionstore/SessionStore.jsm', {});
+var { TabState } = Cu.import('resource:///modules/sessionstore/TabState.jsm', {});
+var { TabStateCache } = Cu.import('resource:///modules/sessionstore/TabStateCache.jsm', {});
+
+function isInternalAPIsAvailable() {
+	if (!SessionStoreInternal) {
+		Components.utils.reportError(new Error('suspendtab: Failed to load SessionStoreInternal'));
+		return false;
+	}
+
+	if (!TabState) {
+		Components.utils.reportError(new Error('suspendtab: Failed to load TabState'));
+		return false;
+	}
+
+	if (!TabStateCache) {
+		Components.utils.reportError(new Error('suspendtab: Failed to load TabStateCache'));
+		return false;
+	}
+
+	return true;
+}
+
+var fullStates = new WeakMap();
+
+function SuspendTabInternal(aWindow)
+{
+	this.init(aWindow);
+}
+SuspendTabInternal.prototype = {
+	__proto__ : require('const'),
+
+	get debug()
+	{
+		return prefs.getPref(this.domain + 'debug');
+	},
+
+	get saferSuspend()
+	{
+		return prefs.getPref(this.domain + 'saferSuspend');
+	},
+
+	get document()
+	{
+		return this.window.document;
+	},
+
+	init : function(aWindow)
+	{
+		SuspendTabInternal.instances.push(this);
+		this.window = aWindow;
+	},
+
+	destroy : function(aWindow)
+	{
+		delete this.window;
+		if (SuspendTabInternal)
+			SuspendTabInternal.instances.splice(SuspendTabInternal.instances.indexOf(this), 1);
+	},
+
+	isSuspended : function(aTab)
+	{
+		return this.isSuspendedBySelf(aTab) || this.isSuspendedBySS(aTab);
+	},
+
+	isSuspendedBySelf : function(aTab)
+	{
+		return SS.getTabValue(aTab, this.STATE);
+	},
+
+	isSuspendedBySS : function(aTab)
+//	isTabNeedToBeRestored: function(aTab)
+	{
+		var browser = aTab.linkedBrowser;
+//		// Firefox 25 and later. See: https://bugzilla.mozilla.org/show_bug.cgi?id=867142
+//		if (TabRestoreStates &&
+//			TabRestoreStates.has(browser))
+//			return TabRestoreStates.isNeedsRestore(browser);
+
+		return browser.__SS_restoreState == 1;
+	},
+
+	suspend : function(aTab)
+	{
+		if (this.isSuspended(aTab))
+			return true;
+
+		let (event = this.document.createEvent('Events')) {
+			event.initEvent(this.EVENT_TYPE_SUSPENDING, true, true);
+			if (!aTab.dispatchEvent(event))
+				return false;
+		}
+
+		if (this.debug)
+			dump(' suspend '+aTab._tPos+'\n');
+
+		var browser = aTab.linkedBrowser;
+
+		TabState.flush(browser);
+		var state = TabState.clone(aTab);
+		fullStates.set(aTab, state);
+
+		// We only need minimum data required to restore the session history,
+		// so drop needless information.
+		var partialState = {
+			entries   : state.entries,
+			storage   : state.storage || null,
+			index     : state.index,
+			pageStyle : state.pageStyle || null
+		};
+		SS.setTabValue(aTab, this.STATE, JSON.stringify(partialState));
+
+		var label = aTab.label;
+		var SHistory = browser.sessionHistory;
+		var uri = browser.currentURI.clone();
+		var self = this;
+		browser.addEventListener('load', function onLoad() {
+			browser.removeEventListener('load', onLoad, true);
+
+			aTab.setAttribute('label', label);
+			aTab.setAttribute('visibleLabel', label);
+			if (self.debug)
+				aTab.setAttribute('tooltiptext', label +' (suspended)');
+
+			// Because Firefox sets the default favicon on this event loop,
+			// we have to reset the favicon in the next loop.
+			timer.setTimeout(function() {
+				aTab.setAttribute('image', state.attributes.image || state.image);
+			}, 0);
+
+			browser.docShell.setCurrentURI(uri);
+			browser.contentDocument.title = label;
+
+			// Don't purge all histories - leave the last one!
+			// The SS module stores the title of the history entry
+			// as the title of the restored tab.
+			// If there is no history entry, Firefox will restore
+			// the tab with the default title (the URI of the page).
+			if (SHistory.count > 1)
+				SHistory.PurgeHistory(SHistory.count - 1);
+
+			aTab.setAttribute('pending', true);
+			aTab.setAttribute(self.SUSPENDED, true);
+
+			if (self.saferSuspend) {
+				if (self.debug)
+					dump(' => ready to restore '+aTab._tPos+'\n');
+				self.readyToResume(aTab);
+			}
+
+			let (event = self.document.createEvent('Events')) {
+				event.initEvent(self.EVENT_TYPE_SUSPENDED, true, false);
+				aTab.dispatchEvent(event);
+			}
+		}, true);
+
+		// Load a blank page to clear out the current history entries.
+		browser.loadURI('about:blank');
+
+		return true;
+	},
+
+	resume : function(aTabs)
+	{
+		if (aTabs instanceof Ci.nsIDOMElement)
+			aTabs = [aTabs];
+
+		aTabs.forEach(function(aTab) {
+			this.resumeOne(aTab);
+		}, this);
+	},
+
+	resumeOne : function(aTab, aIdMap, aDocIdentMap)
+	{
+		if (!this.isSuspended(aTab))
+			return true;
+
+		if (this.isSuspendedBySS(aTab)) {
+			// Reloading action resumes the pending restoration.
+			// This will fire "SSTabRestored" event, then this method
+			// will be called again to restore actual history entries.
+			aTab.linkedBrowser.reload();
+			return true;
+		}
+
+		let (event = this.document.createEvent('Events')) {
+			event.initEvent(this.EVENT_TYPE_RESUMING, true, true);
+			if (!aTab.dispatchEvent(event))
+				return false;
+		}
+
+		var state = this.getTabState(aTab, true);
+		if (!state)
+			return true;
+
+		delete aTab[this.READY];
+		fullStates.delete(aTab);
+
+		this.readyToResume(aTab);
+		SessionStoreInternal.restoreTabContent(aTab);
+
+		var event = this.document.createEvent('Events');
+		event.initEvent(this.EVENT_TYPE_RESUMED, true, false);
+		aTab.dispatchEvent(event);
+
+		if (this.debug)
+			aTab.setAttribute('tooltiptext', aTab.label);
+	},
+
+	getTabState : function(aTab, aClear)
+	{
+		var state = SS.getTabValue(aTab, this.STATE);
+		if (!state)
+			return null;
+
+		if (aClear)
+			SS.setTabValue(aTab, this.STATE, '');
+
+		return fullStates.get(aTab) || JSON.parse(state);
+	},
+
+	// This restores history entries, but they don't eat the RAM
+	// because Firefox doesn't build DOM tree until they are actually loaded.
+	readyToResume : function(aTab)
+	{
+		if (!this.isSuspended(aTab) ||
+			aTab[this.READY])
+			return true;
+
+		if (this.isSuspendedBySS(aTab))
+			return true;
+
+		var state = this.getTabState(aTab);
+		if (!state)
+			return true;
+
+		var browser = aTab.linkedBrowser;
+		var tabbrowser = this.window.gBrowser;
+
+
+		// ==BEGIN==
+		// these codes are imported from SessionStoreInternal.restoreTabs()
+
+		// Ensure the index is in bounds.
+		let activeIndex = (state.index || state.entries.length) - 1;
+		activeIndex = Math.min(activeIndex, state.entries.length - 1);
+		activeIndex = Math.max(activeIndex, 0);
+
+		// Save the index in case we updated it above.
+		state.index = activeIndex + 1;
+
+		// In electrolysis, we may need to change the browser's remote
+		// attribute so that it runs in a content process.
+		let activePageData = state.entries[activeIndex] || null;
+		let uri = activePageData ? activePageData.url || null : null;
+		tabbrowser.updateBrowserRemoteness(browser, uri);
+
+		// Start a new epoch and include the epoch in the restoreHistory
+		// message. If a message is received that relates to a previous epoch, we
+		// discard it.
+		let epoch = SessionStoreInternal._nextRestoreEpoch++;
+		SessionStoreInternal._browserEpochs.set(browser.permanentKey, epoch);
+
+		// keep the data around to prevent dataloss in case
+		// a tab gets closed before it's been properly restored
+		browser.__SS_data = state;
+		browser.__SS_restoreState = TAB_STATE_NEEDS_RESTORE;
+		browser.setAttribute("pending", "true");
+		aTab.setAttribute("pending", "true");
+
+		// Update the persistent tab state cache with |state| information.
+		TabStateCache.update(browser, {
+		  history: {entries: state.entries, index: state.index},
+		  scroll: state.scroll || null,
+		  storage: state.storage || null,
+		  formdata: state.formdata || null,
+		  disallow: state.disallow || null,
+		  pageStyle: state.pageStyle || null
+		});
+
+		browser.messageManager.sendAsyncMessage("SessionStore:restoreHistory",
+		                                        {tabData: state, epoch: epoch});
+		// ==END==
+
+
+		aTab[this.READY] = true;
+
+		return true;
+	},
+
+	resumeAll : function(aRestoreOnlySuspendedByMe)
+	{
+		Array.forEach(this.tabs, function(aTab) {
+			this.cancelTimer(aTab);
+			if (!aRestoreOnlySuspendedByMe ||
+				aTab.getAttribute(this.SUSPENDED) == 'true')
+				this.resume(aTab);
+		}, this);
+	}
+};
+SuspendTabInternal.isAvailable = isInternalAPIsAvailable;
+
+SuspendTabInternal.instances = [];
+
+function shutdown(aReason)
+{
+	SuspendTabInternal.instances.forEach(function(aInstance) {
+		if (aReason == 'ADDON_DISABLE')
+			aInstance.resumeAll(true);
+		aInstance.destroy();
+	});
+
+	timer = undefined;
+
+	SS = undefined;
+	SessionStoreInternal = undefined;
+	// TabRestoreStates = undefined;
+	TabState = undefined;
+	TabStateCache = undefined;
+
+	fullStates = undefined;
+
+	SuspendTabInternal = undefined;
+
+	shutdown = undefined;
+}
--- modules/suspendtab.js
+++ modules/suspendtab.js
@@ -44,91 +44,13 @@ var timer = require('lib/jstimer');
 var bundle = require('lib/locale')
 				.get('chrome://suspendtab/locale/label.properties');
 
-var SS = Cc['@mozilla.org/browser/sessionstore;1']
-			.getService(Ci.nsISessionStore);
-
-var internalSS = (function() {;
-	var ns = {
-			atob : atob,
-			btoa : btoa
-		};
-	try {
-		Cc['@mozilla.org/moz/jssubscript-loader;1']
-			.getService(Ci.mozIJSSubScriptLoader)
-			.loadSubScript('resource:///modules/sessionstore/SessionStore.jsm', ns);
-		if (ns.SessionStoreInternal._initPrefs)
-			ns.SessionStoreInternal._initPrefs();
-		return ns.SessionStoreInternal;
-	}
-	catch(e) {
-		try {
-			Cc['@mozilla.org/moz/jssubscript-loader;1']
-				.getService(Ci.mozIJSSubScriptLoader)
-				.loadSubScript('resource://gre/components/nsSessionStore.js', ns);
-			return new ns.SessionStoreService();
-		}
-		catch(e) {
-			return null;
-		}
-	}
-})();
-
-var SessionHistoryInternal = (function() {;
-	try {
-		var ns = Cu.import('resource:///modules/sessionstore/SessionHistory.jsm', {});
-		return ns.SessionHistoryInternal;
-	}
-	catch(e) {
-		return null;
-	}
-})();
-
-var ContentRestore = (function() {;
-	try {
-		var ns = Cu.import('resource:///modules/sessionstore/ContentRestore.jsm', {});
-		return ns.ContentRestore;
-	}
-	catch(e) {
-		return null;
-	}
-})();
-
-function isInternalAPIsAvailable() {
-	if (!internalSS) {
-		Components.utils.reportError(new Error('suspendtab: Failed to load internal SessionStore service'));
-		return false;
-	}
-
-	if (ContentRestore) { // Firefox 29 and later
-		if (!ContentRestore) {
-			Components.utils.reportError(new Error('suspendtab: ContentRestore does not defined'));
-			return false;
-		}
-	}
-	else { // Firefox 28 and older
-		if (!internalSS.restoreDocument) {
-			Components.utils.reportError(new Error('suspendtab: SessionStore service does not have restoreDocument() method'));
-			return false;
-		}
-	}
-
-	if (SessionHistoryInternal) { // Firefox 28 and later
-		if (!SessionHistoryInternal.deserializeEntry) {
-			Components.utils.reportError(new Error('suspendtab: SessionHistoryInternal does not have deserializeEntry() method'));
-			return false;
-		}
-	}
-	else { // Firefox 27 and older
-		if (!internalSS._deserializeHistoryEntry) {
-			Components.utils.reportError(new Error('suspendtab: SessionStore service does not have _deserializeHistoryEntry() method'));
-			return false;
-		}
-	}
-
-	return true;
+Cu.import('resource://gre/modules/Services.jsm');
+if (Services.vc.compare(Services.appinfo.version, '25.0') < 0) {
+	load('suspendtab-internal-24');
 }
-
-var fullStates = {};
+else {
+	load('suspendtab-internal');
+}
 
 function SuspendTab(aWindow)
 {
@@ -153,10 +75,6 @@ SuspendTab.prototype = {
 	get autoSuspendResetOnReload()
 	{
 		return prefs.getPref(this.domain + 'autoSuspend.resetOnReload');
-	},
-	get saferSuspend()
-	{
-		return prefs.getPref(this.domain + 'saferSuspend');
 	},
 
 	get document()
@@ -215,9 +133,6 @@ SuspendTab.prototype = {
 
 			case 'TabSelect':
 				return this.onTabSelect(aEvent);
-
-			case 'TabClose':
-				return this.onTabClose(aEvent);
 
 			case 'SSTabRestoring':
 				return this.cancelTimer(aEvent.originalTarget);
@@ -336,7 +251,7 @@ SuspendTab.prototype = {
 			let tabs = Array.filter(this.tabs, function(aTab) {
 					return (
 						aTab.hidden &&
-						!SS.getTabValue(aOtherTab, this.STATE) // skip suspending tabs
+						!this.internal.getTabState(aOtherTab) // skip suspending tabs
 					);
 				});
 			return tabs.length ? tabs[0] : null ;
@@ -348,7 +263,7 @@ SuspendTab.prototype = {
 
 		// skip suspending tabs
 		tabs = tabs.filter(function(aOtherTab) {
-			return !SS.getTabValue(aOtherTab, this.STATE);
+			return !this.internal.getTabState(aOtherTab);
 		}, this);
 		if (!tabs.length)
 			return null;
@@ -369,14 +284,6 @@ SuspendTab.prototype = {
 		this.cancelTimer(tab);
 		this.resume(tab);
 		this.setTimers();
-	},
-
-	onTabClose : function(aEvent)
-	{
-		var tab = aEvent.originalTarget;
-		var id = tab.getAttribute('linkedpanel');
-		if (id in fullStates)
-			delete fullStates[id];
 	},
 
 	/**
@@ -521,12 +428,7 @@ SuspendTab.prototype = {
 
 	resumeAll : function(aRestoreOnlySuspendedByMe)
 	{
-		Array.forEach(this.tabs, function(aTab) {
-			this.cancelTimer(aTab);
-			if (!aRestoreOnlySuspendedByMe ||
-				aTab.getAttribute(this.SUSPENDED) == 'true')
-				this.resume(aTab);
-		}, this);
+		this.internal.resumeAll(aRestoreOnlySuspendedByMe);
 	},
 
 	reserveGC : function()
@@ -555,13 +457,13 @@ SuspendTab.prototype = {
 	{
 		SuspendTab.instances.push(this);
 
-		if (!isInternalAPIsAvailable()) return;
+		if (!SuspendTabInternal.isAvailable()) return;
 
 		this.window = aWindow;
+		this.internal = new SuspendTabInternal(aWindow);
 
 		this.window.addEventListener('unload', this, false);
 		this.window.addEventListener('TabSelect', this, true);
-		this.window.addEventListener('TabClose', this, true);
 		this.window.addEventListener('SSTabRestoring', this, true);
 		this.window.addEventListener('SSTabRestored', this, true);
 		this.browser.addEventListener('load', this, true);
@@ -643,7 +545,6 @@ SuspendTab.prototype = {
 
 			this.window.removeEventListener('unload', this, false);
 			this.window.removeEventListener('TabSelect', this, true);
-			this.window.removeEventListener('TabClose', this, true);
 			this.window.removeEventListener('SSTabRestoring', this, true);
 			this.window.removeEventListener('SSTabRestored', this, true);
 			this.browser.removeEventListener('load', this, true);
@@ -652,7 +553,8 @@ SuspendTab.prototype = {
 			delete this.window;
 		}
 
-		delete this._SessionStoreNS;
+		if (this.internal)
+			delete this.internal;
 
 		if (SuspendTab)
 			SuspendTab.instances.splice(SuspendTab.instances.indexOf(this), 1);
@@ -674,53 +576,7 @@ SuspendTab.prototype = {
 
 	isSuspended : function(aTab)
 	{
-		return this.isSuspendedBySelf(aTab) || this.isSuspendedBySS(aTab);
-	},
-
-	isSuspendedBySelf : function(aTab)
-	{
-		return SS.getTabValue(aTab, this.STATE);
-	},
-
-	isSuspendedBySS : function(aTab)
-//	isTabNeedToBeRestored: function(aTab)
-	{
-		var browser = aTab.linkedBrowser;
-		// Firefox 25 and later. See: https://bugzilla.mozilla.org/show_bug.cgi?id=867142
-		if (this.TabRestoreStates &&
-			this.TabRestoreStates.has(browser))
-			return this.TabRestoreStates.isNeedsRestore(browser);
-
-		return browser.__SS_restoreState == 1;
-	},
-	get TabRestoreStates() {
-		return this.SessionStoreNS.TabRestoreStates;
-	},
-	get SessionStoreNS() {
-		if (!this._SessionStoreNS) {
-			try {
-				// resource://app/modules/sessionstore/SessionStore.jsm ?
-				this._SessionStoreNS = Components.utils.import('resource:///modules/sessionstore/SessionStore.jsm', {});
-			}
-			catch(e) {
-				this._SessionStoreNS = {};
-			}
-		}
-		return this._SessionStoreNS;
-	},
-
-	get TabState() {
-		if (!this._TabState) {
-			try {
-				let TabStateNS = {};
-				Components.utils.import('resource:///modules/sessionstore/TabState.jsm', TabStateNS);
-				this._TabState = TabStateNS.TabState;
-			}
-			catch(e) {
-				this._TabState = {};
-			}
-		}
-		return this._TabState;
+		return this.internal.isSuspended(aTab);
 	},
 
 	suspend : function(aTab)
@@ -728,107 +584,14 @@ SuspendTab.prototype = {
 		if (this.isSuspended(aTab))
 			return true;
 
-		let (event = this.document.createEvent('Events')) {
-			event.initEvent(this.EVENT_TYPE_SUSPENDING, true, true);
-			if (!aTab.dispatchEvent(event))
-				return false;
+		if (!this.internal.suspend(aTab))
+			return false;
+
+		if (aTab.selected) {
+			let nextFocused = this.getNextFocusedTab(aTab);
+			if (nextFocused)
+				this.browser.selectedTab = nextFocused;
 		}
-
-		if (this.debug)
-			dump(' suspend '+aTab._tPos+'\n');
-
-		var label = aTab.label;
-
-		// First, get the current tab state via the genuine step.
-		// We store it to the session data permanently.
-		var state = SS.getTabState(aTab);
-		state = JSON.parse(state);
-		// We only need minimum data required to restore the session history,
-		// so drop needless information.
-		var partialState = {
-			entries   : state.entries,
-			storage   : state.storage || null,
-			index     : state.index,
-			pageStyle : state.pageStyle || null
-		};
-		SS.setTabValue(aTab, this.STATE, JSON.stringify(partialState));
-
-		// If possible, we should use full tab state including sensitive data.
-		// Store it to the volatile storage instaed of the session data, for privacy.
-		let stateWithPrivateData;
-		if (this.TabState && this.TabState._collectSyncUncached) { // Firefox 28 and later
-			stateWithPrivateData = this.TabState._collectSyncUncached(aTab, { includePrivateData: true });
-		}
-		else if (internalSS._collectTabData) { // Firefox 27 and olders
-			stateWithPrivateData = internalSS._collectTabData(aTab, true); // the second argument is ignored on Firefox 25 and later...
-			if (internalSS._updateTextAndScrollDataForTab) {
-				if (internalSS._updateTextAndScrollDataForTab.length == 4) // Firefox 24 and olders
-					internalSS._updateTextAndScrollDataForTab(this.window, aTab.linkedBrowser, stateWithPrivateData, true);
-				else
-					internalSS._updateTextAndScrollDataForTab(aTab, stateWithPrivateData, { includePrivateData: true });
-			}
-		}
-		if (stateWithPrivateData) {
-			fullStates[aTab.getAttribute('linkedpanel')] = JSON.stringify({
-				entries   : stateWithPrivateData.entries,
-				storage   : stateWithPrivateData.storage || null,
-				index     : stateWithPrivateData.index,
-				pageStyle : stateWithPrivateData.pageStyle || null
-			});
-		}
-
-		// OK, let's destroy the current session history!
-		var browser = aTab.linkedBrowser;
-		var SHistory = browser.sessionHistory;
-		var uri = browser.currentURI.clone();
-		var self = this;
-		browser.addEventListener('load', function() {
-			browser.removeEventListener('load', arguments.callee, true);
-
-			aTab.setAttribute('label', label);
-			aTab.setAttribute('visibleLabel', label);
-			if (self.debug)
-				aTab.setAttribute('tooltiptext', label +' (suspended)');
-
-			// Because Firefox sets the default favicon on this event loop,
-			// we have to reset the favicon in the next loop.
-			timer.setTimeout(function() {
-				aTab.setAttribute('image', state.attributes.image || state.image);
-			}, 0);
-
-			browser.docShell.setCurrentURI(uri);
-			browser.contentDocument.title = label;
-
-			// Don't purge all histories - leave the last one!
-			// The SS module stores the title of the history entry
-			// as the title of the restored tab.
-			// If there is no history entry, Firefox will restore
-			// the tab with the default title (the URI of the page).
-			if (SHistory.count > 1) SHistory.PurgeHistory(SHistory.count - 1);
-
-			if (self.saferSuspend) {
-				if (self.debug)
-					dump(' => ready to restore '+aTab._tPos+'\n');
-				self.readyToResume(aTab);
-			}
-
-			aTab.setAttribute('pending', true);
-			aTab.setAttribute(self.SUSPENDED, true);
-
-			if (aTab.selected) {
-				let nextFocused = self.getNextFocusedTab(aTab);
-				if (nextFocused)
-					self.browser.selectedTab = nextFocused;
-			}
-
-			let (event = self.document.createEvent('Events')) {
-				event.initEvent(self.EVENT_TYPE_SUSPENDED, true, false);
-				aTab.dispatchEvent(event);
-			}
-		}, true);
-		// Load a blank page to clear out the current history entries.
-		browser.loadURI('about:blank');
-
 		this.reserveGC();
 
 		return true;
@@ -836,196 +599,7 @@ SuspendTab.prototype = {
 
 	resume : function(aTabs)
 	{
-		if (aTabs instanceof Ci.nsIDOMElement) aTabs = [aTabs];
-		var idMap = { used : {} };
-		var docIdentMap = {};
-		aTabs.forEach(function(aTab) {
-			this.resumeOne(aTab, idMap, docIdentMap);
-		}, this);
-	},
-
-	resumeOne : function(aTab, aIdMap, aDocIdentMap)
-	{
-		if (!this.isSuspended(aTab))
-			return true;
-
-		if (this.isSuspendedBySS(aTab)) {
-			// Reloading action resumes the pending restoration.
-			// This will fire "SSTabRestored" event, then this method
-			// will be called again to restore actual history entries.
-			aTab.linkedBrowser.reload();
-			return true;
-		}
-
-		let (event = this.document.createEvent('Events')) {
-			event.initEvent(this.EVENT_TYPE_RESUMING, true, true);
-			if (!aTab.dispatchEvent(event))
-				return false;
-		}
-
-		this.readyToResume(aTab, aIdMap, aDocIdentMap);
-
-		var state = this.getTabState(aTab, true);
-		if (!state)
-			return true;
-
-		var index = aTab[this.INDEX];
-
-		delete aTab[this.INDEX];
-		delete aTab[this.READY];
-
-		if (index > -1) {
-			let self = this;
-			let browser = aTab.linkedBrowser;
-			let SHistory = browser.sessionHistory
-							.QueryInterface(Ci.nsISHistory)
-							.QueryInterface(Ci.nsISHistoryInternal);
-			browser.addEventListener('load', function(aEvent) {
-				if (
-					!aEvent ||
-					!aEvent.originalTarget ||
-					!aEvent.originalTarget.defaultView ||
-					aEvent.originalTarget.defaultView != browser.contentWindow
-					)
-					return;
-
-				browser.removeEventListener('load', arguments.callee, true);
-
-				// Set dummy "tab" because restoreDocument() fires
-				// the SSTabRestored event. We don't need it.
-				browser.__SS_restore_tab = {
-					ownerDocument : aTab.ownerDocument,
-					dispatchEvent : function() {}
-				};
-				// This is required to restore form data.
-				browser.__SS_restore_data = state.entries[index];
-				if (state.pageStyle)
-					browser.__SS_restore_pageStyle = state.pageStyle;
-
-				// Restore form data and scrolled positions.
-				if (ContentRestore && browser.messageManager) { // Firefox 29 and later
-					browser.messageManager.sendAsyncMessage(
-						'SessionStore:restoreHistory',
-						{
-							tabData : state,
-							epoch   : 1
-						}
-					);
-				}
-				else { // Firefox 28 and older
-					internalSS.restoreDocument(browser.ownerDocument.defaultView, browser, aEvent);
-				}
-
-				aTab.removeAttribute('pending');
-				aTab.removeAttribute(self.SUSPENDED);
-
-				let event = self.document.createEvent('Events');
-				event.initEvent(self.EVENT_TYPE_RESUMED, true, false);
-				aTab.dispatchEvent(event);
-			}, true);
-
-			try {
-				// This action loads the page to the RAM, then
-				// Firefox starts to build DOM tree for the page.
-				SHistory.getEntryAtIndex(index, true);
-				SHistory.reloadCurrentEntry();
-			}
-			catch(e) {
-				dump(e+'\n');
-			}
-		}
-		else {
-			let event = this.document.createEvent('Events');
-			event.initEvent(this.EVENT_TYPE_RESUMED, true, false);
-			aTab.dispatchEvent(event);
-		}
-
-		if (this.debug)
-			aTab.setAttribute('tooltiptext', aTab.label);
-	},
-
-	getTabState : function(aTab, aClear)
-	{
-		var state = SS.getTabValue(aTab, this.STATE);
-		if (!state)
-			return null;
-
-		if (aClear)
-			SS.setTabValue(aTab, this.STATE, '');
-
-		// If there is a full tab state in the volatile storage, use it.
-		var id = aTab.getAttribute('linkedpanel');
-		if (id in fullStates) {
-			state = fullStates[id];
-			if (aClear)
-				delete fullStates[id];
-		}
-
-		return JSON.parse(state);
-	},
-
-	// This restores history entries, but they don't eat the RAM
-	// because Firefox doesn't build DOM tree until they are actually loaded.
-	readyToResume : function(aTab, aIdMap, aDocIdentMap)
-	{
-		if (!this.isSuspended(aTab) ||
-			aTab[this.READY])
-			return true;
-
-		if (this.isSuspendedBySS(aTab))
-			return true;
-
-		var state = this.getTabState(aTab);
-		if (!state)
-			return true;
-
-		// First, clear all existing information.
-		// We recycle this tab to load session information.
-		var browser = aTab.linkedBrowser;
-		var SHistory = browser.sessionHistory
-						.QueryInterface(Ci.nsISHistory)
-						.QueryInterface(Ci.nsISHistoryInternal);
-		if (SHistory.count > 0)
-			SHistory.PurgeHistory(SHistory.count);
-
-		// OK, let's restore the tab!
-		browser.stop();
-
-		var index = (state.index || state.entries.length) - 1;
-		if (index >= state.entries.length)
-			index = state.entries.length - 1;
-
-		let current = state.entries[index] || null;
-		let uri = current && current.url || null;
-		browser.userTypedValue = uri;
-
-
-		aIdMap = aIdMap || { used : {} };
-		aDocIdentMap = aDocIdentMap || {};
-		if (SessionHistoryInternal && SessionHistoryInternal.deserializeEntry) { // Firefox 28 and later
-			state.entries.forEach(function(aEntry) {
-				SHistory.addEntry(SessionHistoryInternal.deserializeEntry(aEntry, aIdMap, aDocIdentMap), true);
-			});
-		}
-		else { // Firefox 27 and older
-			state.entries.forEach(function(aEntry) {
-				SHistory.addEntry(internalSS._deserializeHistoryEntry(aEntry, aIdMap, aDocIdentMap), true);
-			});
-		}
-
-		/*
-		// We don't have to restore session storage because we didn't clear it
-		// and it is restoed on the startup, by Firefox itself.
-		if (internalSS._deserializeSessionStorage &&
-			state.storage &&
-			browser.docShell instanceof Ci.nsIDocShell)
-			internalSS._deserializeSessionStorage(state.storage, browser.docShell);
-		*/
-
-		aTab[this.READY] = true;
-		aTab[this.INDEX] = index;
-
-		return true;
+		this.internal.resume(aTabs);
 	}
 };
 
@@ -1049,13 +623,11 @@ function shutdown(aReason)
 	WindowManager = undefined;
 	timer = undefined;
 	bundle = undefined;
-
-	SS = undefined;
-	internalSS = undefined;
-	fullStates = undefined;
+	Services = undefined;
 
 	SuspendTab.instances = undefined;
 	SuspendTab = undefined;
+	SuspendTabInternal = undefined;
 
 	shutdown = undefined;
 }
